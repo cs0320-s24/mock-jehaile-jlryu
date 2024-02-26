29d2bbe21a6db7f9e101fdcca243897b6c1ee4f9
Jowet <jowet@jowets-air-1161.devices.brown.edu>
2024-02-26 17:37:20 -0500

    feat: set up load method

---
+++ data/MockedJSON.tsx
@@ -0,0 +1,18 @@
+type fileRecord = Record<string, number>;
+
+// const myRecord: fileRecord = {
+//   key1: 1,
+//   key2: 2,
+//   key3: 3,
+// };
+
+var fileNames:string[]; 
+fileNames = ["exampleCSV1", "exampleCSV2", "exampleCSV3", "exampleCSV4"];
+
+
+const exampleCSV1 = [
+    [1, 2, 3, 4, 5],
+    ["The", "song", "remains", "the", "same."],
+]
+
+
--- src/components/REPLCommands.tsx
+++ src/components/REPLCommands.tsx
@@ -1,8 +1,11 @@
 // Define a type for the response of command execution
+import { fileNames } from '/Users/jowet/Desktop/cs320/mock-jehaile-jlryu/data/MockedJSON';
+
 type CommandResponse = {
     isValid: boolean;
     response: string;
   };
+  var loadedFile:string[];
   
   // Mocked command execution for demonstration
   const executeCommand = (command: string): CommandResponse => {
@@ -10,25 +13,29 @@ type CommandResponse = {
         // Assuming command is a string with the format "load_file <csv-file-path>"
         const parts = command.split(' '); // Split the command by spaces
         const filePath = parts[1]; 
-        if (filePath){
-          return { isValid: true, response: `Loading file from path: ${filePath}` };
+        if (filePath && fileNames.includes(filePath)){
+
+            loadedFile.push(filePath);
+
+          return {isValid: true, response: "Loading file from path:" + {filePath}};
+
+        
         }
+        return {isValid:false, response: "Invalid file name. Reenter a valid file name."}
       }
-    // // Example: Adding logic to check for and execute a "load_file" command
-    // if (command.startsWith("load_file")) {
-    //   const filePath = command.substring("load_file".length).trim();
-    //   if (filePath) {
-    //     // Here, you would add your logic to load the file based on filePath
-    //     // For now, we'll just return a success message
-    //     return { isValid: true, response: `Loading file from path: ${filePath}` };
-    //   }
+      if(command.startsWith("view")){
+        //if they load a file then type invalid command then want to view the previous should that work?
+        //if they load a file and then 
+
+      }
     }
+    const executeView()
   
     // Add more commands and their handling logic here
     
     // Default response for unknown commands
-    return { isValid: false, response: "Unknown command" };
-  };
+    // return { isValid: false, response: "Unknown command" };
+  
   
   export { executeCommand, CommandResponse };
   
